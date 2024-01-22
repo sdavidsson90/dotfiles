82626c1049ba3537996ce2c9dd9402834cfd4e72
Sighvatur Sveinn Davidsson <sdavidsson90@gmail.com>
2024-01-22 15:48:58 +0100

    17954

---
+++ linux_desktop/copy_tools.sh
@@ -0,0 +1,104 @@
+# COPY TOOLS
+# Compatible with bash and zsh
+
+# Linux:
+# xsel -ob: paste
+# xsel -ib: copy
+
+# ----------------
+# Copy working directory
+cpwd() {
+  printf $(dirs) | xsel -ib
+}
+
+# ----------------
+# Copy file contents
+cpfc() {
+  filename=$(basename $1)
+  echo -e "\e[1mCopied content of:\e[0m $filename"
+  cat $1 | xsel -ib
+}
+
+
+# ----------------
+# Copy filepath
+cpfp() {
+  filepath=$(realpath -z $1)
+  echo -n $filepath | xsel -ib
+  echo -e "\e[1mCopied to clipboard:\e[0m $filepath"
+}
+
+
+# ----------------
+# Paste as string
+p() {
+  echo -en "\e[1mClipboard content: \e[0m"
+  content=$(xsel -ob)
+  if [ -z $content ]; then
+    echo "[empty]"
+  else
+    echo $content
+  fi
+}
+
+
+# ----------------
+# Execute string in copybuffer as command
+pp() {
+  echo -e "\e[1mClipboard content:\e[0m $(xsel -ob)"
+  echo -en "\e[1mDo you want to execute this as a command? [Y/n]:\e[0m "
+  local response=$(bash -c "read -n 1 response; echo \$response")
+  if [ ! -z $response ]; then
+    local RESPONSE=$response
+  else
+    local RESPONSE='Y'
+  fi
+  case $RESPONSE in
+    [Yy])
+      echo -e "\n--"
+      if ! eval $(xsel -ob) 2>/dev/null; then
+        echo "Not a valid command"
+        return 1
+      fi
+      ;;
+    [Nn])
+      echo -e "\nNo action taken!"
+      ;;
+    *)
+      echo -e "\nInvalid response!"
+      return 1
+      ;;
+  esac
+}
+
+
+# ----------------
+# Paste to directory
+pfp() {
+  FILENAME=$(basename $(xsel -ob))
+  if [ -e $FILENAME ]; then
+    echo -en "\e[1m$FILENAME already exists! Do you wish to overwrite? [Y/n]:\e[0m "
+    local response=$(bash -c "read -n 1 response; echo \$response")
+  fi
+  if [ ! -z $response ]; then
+    local RESPONSE=$response
+  else
+    local RESPONSE='Y'
+  fi
+  case $RESPONSE in
+    [Yy]) 
+        FILE=$(xsel -ob)
+        cp -r $FILE . 2>/dev/null && \
+        echo -e "\nPasted to current directory!" || \
+        echo -e "\nSomething went wrong:" && \
+        cp -r $FILE .
+      ;;
+    [Nn])
+      echo -e "\nNo action taken!"
+      ;;
+    *)
+      echo -e "\nInvalid response!"
+      return 1
+      ;;
+  esac
+}
